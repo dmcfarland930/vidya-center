fde580cfdbc4265878ee013192ae5ed4d53f6ae7
Daniel McFarland <dmcfarland930@gmail.com>
2023-01-24 19:13:49 -0700

    Added categories to firebase. Created category context to map shop data. Added category-preview compnent. Added categores-preview and category routes for shop sub routes.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />}/>
-          <Route path='shop' element={<Shop />}/>
+          <Route path='shop/*' element={<Shop />}/>
           <Route path='checkout' element={<Checkout />}/>
           <Route path='sign-in' element={<Authentication />}/>
         </Route>
